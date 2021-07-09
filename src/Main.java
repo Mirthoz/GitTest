9cb1cee25313d981a9a066813c8f49d824360afe
Miroslav <pavlov.miroslav.92@gmail.com>
2021-07-09 20:47:43 +0300

    add method spase

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
     }
+    public static void makeSpace(){
+        System.out.println();
+        System.out.println("----------------");
+        System.out.println();
+    }
 }
